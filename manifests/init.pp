625282d3c4596f18074d230daa3da138f45d2db9
Pat Riehecky <riehecky@fnal.gov>
2016-03-29 13:29:03 -0500

    added task cleanup too

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@
 class foreman_reports (
   $max_days_all       = $foreman_reports::params::max_days_all,
   $max_days_noactions = $foreman_reports::params::max_days_noactions,
+  $max_days_task_log  = $foreman_reports::params::max_days_task_log,
   $cron_ensure        = $foreman_reports::params::cron_ensure,
   $cron_dot           = $foreman_reports::params::cron_dot,
 ) inherits foreman_reports::params{
@@ -14,6 +15,7 @@ class foreman_reports (
     content => "#!/bin/bash
 foreman-rake reports:expire days=${max_days_noactions} status=0 >/dev/null 2>&1
 foreman-rake reports:expire days=${max_days_all} >/dev/null 2>&1
+foreman-rake foreman_tasks:cleanup AFTER=${max_days_task_log} >/dev/null 2>&1
 exit 0
 "
   }
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,7 @@
 class foreman_reports::params {
   $max_days_all       = '90'
   $max_days_noactions = '21'
+  $max_days_task_log  = '400'
   $cron_ensure        = 'present'
   $cron_dot           = 'daily'
 }
